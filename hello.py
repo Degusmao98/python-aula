b4389e018166a407136205b0d48b8d8790e0897d
Degusmão98 <douglasgumao98@gmail.com>
2025-05-05 10:35:46 -0300

    outras alterações

--- hello.py
+++ hello.py
@@ -1,3 +1,17 @@
 
+__version__ = '0.0.1'
+__author__ = 'Douglas Gusmao'
+__license__ = 'Unlicense'
+
+import os
+# dunder (__)
+
 # este imprime 'hello world'
-print('hello world')
+current_language = 'it_IT'
+msg = 'hello word'
+
+if current_language == 'pt_BR':  # condição SE
+    msg = 'olá mundo'
+elif current_language == 'it_IT':  # condição SE NÃO SER
+    msg = 'ciano, Mondo!'
+print(msg)
